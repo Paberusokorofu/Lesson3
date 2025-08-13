fd570079000a5cb3a88b541c8104cb396f6eeefa
Paberusokorofu <8273482@mail.ru>
2025-08-13 17:35:33 +0300

    ending Debug

---
+++ lecture_snippets/probe.py
@@ -0,0 +1,11 @@
+print('hi')
+my_family_height = [
+    ['mother', 168],
+    ['father', 186],
+    ['I', 200],
+]
+total_height = my_family_height[0][1]
+total_height += my_family_height[1][1]
+total_height += my_family_height[2][1]
+print('Total height of my family', total_height)
+print('bye')
