329dacd4ce0e48d8cbb01be68d6a9bafd4514efb
Paberusokorofu <8273482@mail.ru>
2025-08-13 18:58:06 +0300

    3 lesson style

--- lecture_snippets/probe.py
+++ lecture_snippets/probe.py
@@ -1,11 +1,13 @@
-print('hi')
-my_family_height = [
-    ['mother', 168],
-    ['father', 186],
-    ['I', 200],
-]
-total_height = my_family_height[0][1]
-total_height += my_family_height[1][1]
-total_height += my_family_height[2][1]
-print('Total height of my family', total_height)
-print('bye')
+px, y = 3, 8
+
+if x == 3:
+    print(42)
+
+if x < 0:
+    if y > 0:
+        print('налево!')
+    else:
+        print('направо!')
+else:
+    print(
+        'стой!')
