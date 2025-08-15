ade48864d066ff98f63ba4d7257ce911479577e0
psokolov@nbiservice.ru <psokolov@nbiservice.ru>
2025-08-15 16:02:04 +0300

    Урок 3.14 прошёл

--- lecture_snippets/probe.py
+++ lecture_snippets/probe.py
@@ -1,9 +1,2 @@
-while True:
-    user_input = input('Введите 42 >> ')
-    result = int(user_input)
-    if result == 42:
-        print('Спасибо за сотрудничество!')
-        break
-    else:
-        print('Я просил 42, а Вы ввели', result, 'Попробуйте еще раз...')
-
+for i in range(100, 600, 50):
+    print(i)
